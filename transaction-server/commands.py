483832ce60ddad6ce15c97d01856f4b1628e1ae9
Jaime Sedore <jaimesedore@gmail.com>
2019-01-27 15:19:54 -0800

    Change functionality of Buy to add Quotes

--- transaction-server/commands.py
+++ transaction-server/commands.py
@@ -10,9 +10,9 @@ def initdb():
     conn = None
     try:
         # Setting connection params:
-        psql_user = 'postgres'
+        psql_user = 'databaseuser'
         psql_db = 'postgres'
-        psql_password = 'gg'
+        psql_password = ''
         psql_server = 'localhost'
         psql_port = 5432
         
@@ -34,19 +34,20 @@ def initdb():
                         'balance FLOAT NOT NULL);                        ')
         conn.commit()
 
-        cursor.execute( 'CREATE TABLE stocks                                               '
-                        '(username VARCHAR(20) references users(username),                 '
-                        'stock_symbol VARCHAR(3) NOT NULL,                                 '
-                        'stock_quantity INT NOT NULL,                                      '
-                        'PRIMARY KEY (username, stock_symbol));                            ')
+        cursor.execute( 'CREATE TABLE stocks                                 '
+                        '(username VARCHAR(20) references users(username),   '
+                        'stock_symbol VARCHAR(3) NOT NULL,                   '
+                        'stock_quantity INT NOT NULL,                        '
+                        'PRIMARY KEY (username, stock_symbol));              ')
         conn.commit()
 
-        cursor.execute( 'CREATE TABLE reserved                                 '
-                        '(username VARCHAR(20) references users(username),     '
-                        'stock_symbol VARCHAR(3) NOT NULL,                     '
-                        'amount FLOAT NOT NULL,                         '
-                        'timestamp TIMESTAMP NOT NULL,                         '
-                        'PRIMARY KEY (username, stock_symbol));                ')                        
+        cursor.execute( 'CREATE TABLE reserved                                      '
+                        '(reservationid SERIAL PRIMARY KEY,    '
+                        'username VARCHAR(20) references users(username),           '
+                        'stock_symbol VARCHAR(3) NOT NULL,                          '
+                        'stock_quantity INT NOT NULL,                               '
+                        'amount FLOAT NOT NULL,                                     '
+                        'timestamp TIMESTAMP NOT NULL);                              ')                        
         conn.commit()
         return cursor, conn
     except (Exception, psycopg2.DatabaseError) as error:
@@ -72,7 +73,8 @@ def add(user_id, amount, cursor, conn):
                 cursor.execute('UPDATE users SET balance = balance + %s where username = %s;', (amount, user_id))
                 conn.commit()
                 return
-        
+         
+
         cursor.execute('INSERT INTO users VALUES (%s, %s)', (user_id, amount))
         conn.commit()
         return
@@ -119,6 +121,8 @@ def quote(user_id, stock_symbol):
 def buy(user_id, stock_symbol, amount, cursor, conn):
     cursor.execute('SELECT username FROM users;')
     conn.commit()
+    
+    price, stock_symbol, user_id, time_of_quote, cryptokey = quote(user_id, stock_symbol)
 
     for i in cursor.fetchall():
         if i[0] == user_id:
@@ -128,30 +132,13 @@ def buy(user_id, stock_symbol, amount, cursor, conn):
             balance = cursor.fetchone()
             if balance[0] >= float(amount):
                 # CAN AFFORD THE STOCK
-                cursor.execute("SELECT username FROM reserved WHERE username = %s and stock_symbol = %s", (user_id, stock_symbol))
+                cursor.execute("UPDATE users SET balance = balance - %s WHERE username = %s;", (int(amount), user_id))
                 conn.commit()
-
-                if cursor.fetchall() == []:
-                    cursor.execute("UPDATE users SET balance = balance - %s WHERE username = %s;", (int(amount), user_id))
-                    conn.commit()
-                    cursor.execute("INSERT INTO reserved VALUES (%s, %s, %s, %s);", (user_id, stock_symbol, amount, '2011-05-16 15:36:38',))
-                    conn.commit() 
-                else:
-                    cursor.execute("UPDATE users SET balance = balance - %s WHERE username = %s;", (int(amount), user_id))
-                    conn.commit()
-                    cursor.execute("UPDATE reserved SET amount = amount + %s where username = %s;", (int(amount), user_id,))
-                    conn.commit() 
-                    cursor.execute("select * from users")
-                    conn.commit()
-                    print(cursor.fetchall())
-                    cursor.execute("select * from reserved")
-                    conn.commit()
-                    print(cursor.fetchall())
-
+                cursor.execute("INSERT INTO reserved (username, stock_symbol, stock_quantity, amount, timestamp) VALUES (%s, %s, %s, %s, now());", (user_id, stock_symbol, int(float(amount)/price), amount,))
+                conn.commit() 
             else:
                 print("Insufficient Funds")
             return
-
     # USER DOESN"T EXIST
     print("User does not exist")
     return 0
