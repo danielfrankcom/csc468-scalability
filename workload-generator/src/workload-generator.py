0f2a896a2ac209dd205acd521a783275acb8c600
Kyle Thorpe <kylejohnthorpe@gmail.com>
2019-01-28 09:27:24 -0800

    Implement new workload algorithm

--- workload-generator/src/workload-generator.py
+++ workload-generator/src/workload-generator.py
@@ -6,6 +6,11 @@ import logging
 import queue
 from timeit import default_timer as timer
 import time
+import tornado.queues
+from tornado import gen
+from tornado.httpclient import AsyncHTTPClient
+from tornado.ioloop import IOLoop
+
 
 logging.basicConfig(level=logging.DEBUG, filename="/src/log", filemode="a+", format='%(asctime)s: %(threadName)s  %(message)s')
 
@@ -14,24 +19,15 @@ def log(info):
     logging.info(info)
 
 # NOTE You must have http:// at the start of the URL
-URL = "http://nginx"
-PRINT_WORK = True
-PRINT_STATUS_CODE = True
-PRINT_TEXT = True
+URL = "http://nginx:80"
 
-STATS = 1
-NUM_THREADS = 10
+STATS = None
 
-
-
-# try:  
-#    NUM_THREADS = int(os.environ["NUM_THREADS"])
-#    STATS = int(os.environ["STATS"])
-# except KeyError: 
-#    print("Please set the environment variables.")
-#    sys.exit(1)
-
-
+try:  
+   STATS = int(os.environ["STATS"])
+except KeyError: 
+   print("Please set the environment variables.")
+   sys.exit(1)
 
 if len(sys.argv) != 2:
     print("Incorrect usage. Example: /{0} <workload file>".format(sys.argv[0]))
@@ -46,11 +42,8 @@ except:
 
 workload = workload_file.readlines()
 
-total_work_count = 0
-work_done = {} # A list containing the amounts of work done by each worker
-
-user_work = {}
-q = queue.Queue()
+urls = []
+work_done = 0
 
 for work in workload:
     arguments = work.split(" ")[1].split(",")
@@ -59,74 +52,56 @@ for work in workload:
     url = "{0}/{1}".format(URL,command)
     for arg in arguments:
         url += "/{0}".format(arg)
-    user = "NO_USER"
-    if command != "dumplog":
-        user = arguments[0]
-    if user not in user_work:
-        user_work[user] = []
-    user_work[user].append(url)
-    total_work_count+=1
-    continue
+    urls.append(url)
 
-def worker_thread():
-    log("Starting worker thread: {0}".format(threading.current_thread().getName()))
-    global work_done
-    identifier = threading.get_ident()
-    work_done[identifier] = 0
-    while True:
-        work = q.get()
-        if work is None:
-            break # Once all of the work has been finished
-        # Do the work
-        for url in work:
-            if PRINT_WORK: log("{0}".format(url))
-            session = requests.Session()
-            session.trust_env = False
-            r = session.get(url)
-            if(str(r.status_code)[0] != "2"):
-                log("ATTENTION for URL: {0}".format(url))
-            if PRINT_STATUS_CODE: log(r.status_code)
-            if PRINT_TEXT: log(r.text+"\n")
-            work_done[identifier] = work_done[identifier] + 1
-        q.task_done()
-
-def timing_thread(starting_count):
+def timing_thread(total_work):
     start = timer()
-    global work_done
     while True:
         elapsed = timer() - start
-        work_done_total = sum(work_done.values())
-        tps = work_done_total/elapsed
-        stats = "[STATS]: {0}/{1} @ {2} transactions per second".format(work_done_total,starting_count,tps)
+        tps = work_done/elapsed
+        stats = "[STATS]: {0}/{1} @ {2} transactions per second".format(work_done,total_work,tps)
         print(stats)
-        if work_done_total >= starting_count:
+        if work_done >= total_work:
             end = timer()
-            finishing_debug = "[STATS]: Completed {0} using {1} threads in {2} seconds.".format(path,NUM_THREADS,(end - start))
+            finishing_debug = "[STATS]: Completed {0} in {1} seconds.".format(path,(end - start))
             log(finishing_debug)
             print(finishing_debug)
             break
         time.sleep(1)
 
-threads = []
-q = queue.Queue()
-# Put each user's work into the queue
-for _,work in user_work.items():
-    q.put(work)
 if STATS:
-    t = threading.Thread(target=timing_thread, args=(total_work_count,))
+    t = threading.Thread(target=timing_thread, args=(len(urls),))
     t.start()
-    threads.append(t)
 
-#TODO: Allow all threads to start and get a workload before the first request is sent.
-for _ in range(NUM_THREADS):
-    t = threading.Thread(target=worker_thread)
-    t.start()
-    threads.append(t)
+NUM_WORKERS = 100
+QUEUE_SIZE = 1000
+q = tornado.queues.Queue(QUEUE_SIZE)
+AsyncHTTPClient.configure(None, max_clients=NUM_WORKERS)
+http_client = AsyncHTTPClient()
 
-q.join() # block until all tasks are complete
+@gen.coroutine
+def worker():
+    global work_done
+    while True:
+        url = yield q.get()
+        try:
+            response = yield http_client.fetch(url)
+            #print('got response from', url)
+            work_done+=1
+        except Exception as e:
+            print('failed to fetch', url)
+            print(e)
+        finally:
+            q.task_done()
 
-# Stop all of the workers
-for _ in range(NUM_THREADS):
-    q.put(None)
-for t in threads:
-    t.join()
+@gen.coroutine
+def main():
+    for i in range(NUM_WORKERS):
+        IOLoop.current().spawn_callback(worker)
+    for url in urls:
+        # When the queue fills up, stop here to wait instead
+        # of reading more from the file.
+        yield q.put(url)
+    yield q.join()
+
+IOLoop.current().run_sync(main)
