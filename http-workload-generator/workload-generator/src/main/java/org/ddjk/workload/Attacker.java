a257acf0db4f97e193956e78eb36b224f3114787
danielfrankcom <danielfrankcom@gmail.com>
2019-01-30 15:35:16 -0800

    Use asynchttpclient to improve throughput to nginx

--- http-workload-generator/workload-generator/src/main/java/org/ddjk/workload/Attacker.java
+++ http-workload-generator/workload-generator/src/main/java/org/ddjk/workload/Attacker.java
@@ -1,12 +1,19 @@
 package org.ddjk.workload;
 
 import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Dsl;
+import org.asynchttpclient.Request;
+import org.asynchttpclient.Response;
 
-import static org.asynchttpclient.Dsl.asyncHttpClient;
+import java.util.concurrent.Future;
+
 
 public class Attacker { 
- 
-    private static int REQUESTS = 26_000;
+
+    private static final int NS_IN_MS = 1_000_000;
+    private static final int MS_IN_S = 1_000;
+
+    private static final int NUM_REQUESTS = 26_000;
     private static final String HOSTNAME = "localhost";
     private static final int PORT = 80;
 
@@ -14,16 +21,36 @@ public class Attacker {
 
     public static void main(String args[]) {
 
-        long start = System.nanoTime();
+        final AsyncHttpClient client = Dsl.asyncHttpClient();
+
+        final Request[] requests = new Request[NUM_REQUESTS];
+
+        for (int i = 0; i < NUM_REQUESTS; i++) {
+            requests[i] = Dsl.get(URL).build();
+        }
+
         System.out.println("started");
 
-        AsyncHttpClient asyncHttpClient = asyncHttpClient();
+        final long durationNS = execute(client, requests);
+        final long durationMS = durationNS / NS_IN_MS;
+        final float durationS = durationMS / (float) MS_IN_S;
 
-        long finish = System.nanoTime();
-        System.out.println("stopped");
-        System.out.println("Finished in: "+(finish - start)/1000000 + "ms");
+        final String output = "Finished in: " + durationS + "s.";
+        System.out.println(output);
 
         System.exit(0);
-    } 
+    }
+
+    private static long execute(AsyncHttpClient client, Request... requests) {
+        long start = System.nanoTime();
+
+        for (Request request : requests) {
+            Future<Response> response = client.executeRequest(request);
+        }
+
+        long finish = System.nanoTime();
+        return finish - start;
+
+    }
  
 } 
