ab00309df1eae95c3d2a6acb324eb3852a1ffc25
danielfrankcom <danielfrankcom@gmail.com>
2019-02-06 23:04:51 -0800

    temp commit - really big

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # csc468-scalability
+
+## Requirements
+
+You must have `docker-compose` installed to run this project.
+
+## Running
+
+Execute `sudo docker-compose up --force-recreate` from the root directory of the project. This will start all of the necessary components for the distributed system.
+
+You can connect to Adminer on `localhost:80`, access the webserver on `localhost:5000`, and manually send POST requests to the transaction server using `localhost:4000`.
+
+### Notes
+
+- You must run `docker compose down` to close down the containers properly. If you do not, next time the containers start up they may be the same instances, and the system will not be in a clean state.
+- If you change the docker files, you must run `sudo docker-compose build` to rebuild the images.
+- If you want the containers to run in the background, use the `-d` flag at the end of the command.
+- If you want to watch a specific container's log files, use `sudo docker-compose logs -f <container name from docker-compose.yml>`.
+- If you want to open a shell in a specific container, use `sudo docker-compose -exec <container name from docker-compose.yml> /bin/sh`.
+- All commands that start with `docker-compose` must be run from the root directory of the project, or the `docker-compose.yml` file will not be accessible.
+
+## Providing Workloads (Linux/MacOS)
+
+To provide a workload to the transaction server, we need to connect to the workload-generator container. There is a script named `run` in the root directory of the project that can be used as a shortcut for this process.
+
+Execute `./run workloads/<name here>` to pump commands through the system.
+
+If the specified workload contains a `DUMPLOG` command, it will be have been created in `transaction-server/out`. 
---
+++ transaction-server/src/server.py
@@ -0,0 +1,133 @@
+import sys, os
+from flask import Flask, request, jsonify
+from lib.commands import *
+
+"""
+Provides a method to call with parameters.
+"""
+def parseCommand(raw):
+    arguments = raw.split(",")
+    command = arguments[0]
+    command = command.upper()
+
+    #QUOTE Command
+    if command == "QUOTE":
+        print("got in quote:", arguments)
+        try:
+            command, user_id, stock_symbol = arguments
+        except ValueError:
+            print("Invalid Input. <QUOTE user_id stock_symbol>")
+        else:    
+            quote(user_id, stock_symbol)
+    #ADD Command
+    elif command == "ADD":
+        try:
+            command, user_id, amount = arguments
+        except ValueError:
+            print("Invalid Input. <ADD, USER_ID, AMOUNT>")
+        else:    
+            add(user_id, amount, cursor, conn)
+    #BUY Command
+    elif command == "BUY":
+        try:
+            command, user_id, stock_symbol, amount = arguments
+        except ValueError:
+            print("Invalid Input. <BUY USER_ID STOCK_SYMBOL AMOUNT>")
+        else:    
+            buy(user_id, stock_symbol, amount, cursor, conn)
+    elif command == "COMMIT_BUY":
+        try:
+            command, user_id = arguments
+        except ValueError:
+            print("Invalid Input. <COMMIT_BUY USER_ID>")
+        else:    
+            commit_buy(user_id, cursor, conn)
+    elif command == "CANCEL_BUY":
+        try:
+            command, user_id = arguments
+        except ValueError:
+            print("Invalid Input. <CANCEL_BUY USER_ID>")
+        else:    
+            cancel_buy(user_id, cursor, conn)
+    elif command == "SELL":
+        try:
+            command, user_id, stock_symbol, amount = arguments
+        except ValueError:
+            print("Invalid Input. <SELL USER_ID STOCK_SYMBOL AMOUNT>")
+        else:    
+            sell(user_id, stock_symbol, amount, cursor, conn)
+    elif command == "COMMIT_SELL":
+        try:
+            command, user_id = arguments
+        except ValueError:
+            print("Invalid Input. <COMMIT_SELL USER_ID>")
+        else:    
+            commit_sell(user_id, cursor, conn)
+    elif command == "SET_BUY_AMOUNT":
+        try:
+            command, user_id, stock_symbol, amount = arguments
+        except ValueError:
+            print("Invalid input.  <SET_BUY_AMOUNT USER_ID STOCK_SYMBOL AMOUNT>")
+        else:
+            set_buy_amount(user_id, stock_symbol, amount, cursor, conn)
+    elif command == "CANCEL_SET_BUY":
+        try:
+            command, user_id, stock_symbol = arguments
+        except ValueError:
+            print("Invalid input.  <CANCEL_SET_BUY USER_ID STOCK_SYMBOL>")
+        else:
+            cancel_set_buy(user_id, stock_symbol, cursor, conn)
+    elif command == "SET_BUY_TRIGGER":
+        try:
+            command, user_id, symbol, amount = arguments
+        except ValueError:
+            print("Invalid input. <SET_BUY_TRIGGER USER_ID STOCK_SYMBOL AMOUNT>")
+        else:
+            set_buy_trigger(user_id, symbol, amount, cursor, conn)
+    elif command == "SET_SELL_AMOUNT":
+        try:
+            command, user_id, stock_symbol, amount = arguments
+        except ValueError:
+            print("Invalid input.  <SET_SELL_AMOUNT USER_ID STOCK_SYMBOL AMOUNT>")
+        else:
+            set_sell_amount(user_id, stock_symbol, amount, cursor, conn)
+    elif command == "CANCEL_SET_SELL":
+        try:
+            command, user_id, stock_symbol = arguments
+        except ValueError:
+            print("Invalid input.  <CANCEL_SET_SELL USER_ID STOCK_SYMBOL>")
+        else:
+            cancel_set_sell(user_id, stock_symbol, cursor, conn)
+    elif command == "SET_SELL_TRIGGER":
+        try:
+            command, user_id, symbol, amount = arguments
+        except ValueError:
+            print("Invalid input. <SET_SELL_TRIGGER USER_ID STOCK_SYMBOL AMOUNT>")
+        else:
+            set_sell_trigger(user_id, symbol, amount, cursor, conn)
+    elif command == "DUMPLOG":
+        try:
+            command, location = arguments
+        except ValueError:
+            print("Invalid input. <SET_SELL_TRIGGER USER_ID STOCK_SYMBOL AMOUNT>")
+        else:
+            filename = os.path.basename(location)
+            path = os.path.join("/out/", filename)
+            dumplog(path)
+
+    else:
+        print(arguments, " Invalid Command")
+
+
+app = Flask(__name__)
+cursor, conn = initdb()
+
+@app.route('/', methods=['POST'])
+def root():
+
+    body = request.data.decode('utf-8')
+    print(body, flush=True)
+    parseCommand(body)
+
+    response = jsonify(success=True)
+    return response
