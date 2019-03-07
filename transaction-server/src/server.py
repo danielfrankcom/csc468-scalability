47afa5d64d578f4d9b79a700acda9ffed468882e
danielfrankcom <danielfrankcom@gmail.com>
2019-03-06 17:02:34 -0800

    Removing unnecessary components for testing purposes

--- transaction-server/src/server.py
+++ transaction-server/src/server.py
@@ -1,9 +1,7 @@
-import sys, os, re
+import re, time
+
 from flask import Flask, request, jsonify
 from lib.commands import *
-
-from queue import Queue
-import time
 import psycopg2
 from psycopg2 import pool
 
@@ -12,7 +10,7 @@ pattern = re.compile(r"^\[(\d+)\] ([A-Z_]+),([^ ]+) ?$")
 """
 Provides a method to call with parameters.
 """
-def parseCommand(raw, cursor, conn):
+def parseCommand(raw, conn):
 
     match = re.findall(pattern, raw)
 
@@ -23,144 +21,19 @@ def parseCommand(raw, cursor, conn):
     transactionNum, command, arguments = match[0]
     transactionNum = int(transactionNum)
     arguments = arguments.split(",")
-    
-    #QUOTE Command
-    if command == "QUOTE":
-        print("got in quote:", arguments)
-        try:
-            user_id, stock_symbol = arguments
-        except ValueError:
-            print("Invalid Input. <QUOTE user_id stock_symbol>")
-        else:    
-            quote(transactionNum, user_id, stock_symbol)
+
     #ADD Command
-    elif command == "ADD":
+    if command == "ADD":
         try:
             user_id, amount = arguments
         except ValueError:
             print("Invalid Input. <ADD, USER_ID, AMOUNT>")
         else:    
-            add(transactionNum, user_id, amount, cursor, conn)
-    #BUY Command
-    elif command == "BUY":
-        try:
-            user_id, stock_symbol, amount = arguments
-        except ValueError:
-            print("Invalid Input. <BUY USER_ID STOCK_SYMBOL AMOUNT>")
-        else:    
-            buy(transactionNum, user_id, stock_symbol, amount, cursor, conn)
-    elif command == "COMMIT_BUY":
-        try:
-            [user_id] = arguments
-        except ValueError:
-            print("Invalid Input. <COMMIT_BUY USER_ID>")
-        else:    
-            commit_buy(transactionNum, user_id, cursor, conn)
-    elif command == "CANCEL_BUY":
-        try:
-            [user_id] = arguments
-        except ValueError:
-            print("Invalid Input. <CANCEL_BUY USER_ID>")
-        else:    
-            cancel_buy(transactionNum, user_id, cursor, conn)
-    elif command == "SELL":
-        try:
-            user_id, stock_symbol, amount = arguments
-        except ValueError:
-            print("Invalid Input. <SELL USER_ID STOCK_SYMBOL AMOUNT>")
-        else:    
-            sell(transactionNum, user_id, stock_symbol, amount, cursor, conn)
-    elif command == "COMMIT_SELL":
-        try:
-            [user_id] = arguments
-        except ValueError:
-            print("Invalid Input. <COMMIT_SELL USER_ID>")
-        else:    
-            commit_sell(transactionNum, user_id, cursor, conn)
-    elif command == "SET_BUY_AMOUNT":
-        try:
-            user_id, stock_symbol, amount = arguments
-        except ValueError:
-            print("Invalid input.  <SET_BUY_AMOUNT USER_ID STOCK_SYMBOL AMOUNT>")
-        else:
-            set_buy_amount(transactionNum, user_id, stock_symbol, amount, cursor, conn)
-    elif command == "CANCEL_SET_BUY":
-        try:
-            user_id, stock_symbol = arguments
-        except ValueError:
-            print("Invalid input.  <CANCEL_SET_BUY USER_ID STOCK_SYMBOL>")
-        else:
-            cancel_set_buy(transactionNum, user_id, stock_symbol, cursor, conn)
-    elif command == "SET_BUY_TRIGGER":
-        try:
-            user_id, symbol, amount = arguments
-        except ValueError:
-            print("Invalid input. <SET_BUY_TRIGGER USER_ID STOCK_SYMBOL AMOUNT>")
-        else:
-            set_buy_trigger(transactionNum, user_id, symbol, amount, cursor, conn)
-    elif command == "SET_SELL_AMOUNT":
-        try:
-            user_id, stock_symbol, amount = arguments
-        except ValueError:
-            print("Invalid input.  <SET_SELL_AMOUNT USER_ID STOCK_SYMBOL AMOUNT>")
-        else:
-            set_sell_amount(transactionNum, user_id, stock_symbol, amount, cursor, conn)
-    elif command == "CANCEL_SET_SELL":
-        try:
-            user_id, stock_symbol = arguments
-        except ValueError:
-            print("Invalid input.  <CANCEL_SET_SELL USER_ID STOCK_SYMBOL>")
-        else:
-            cancel_set_sell(transactionNum, user_id, stock_symbol, cursor, conn)
-    elif command == "CANCEL_SELL":
-        try:
-            [user_id] = arguments
-        except ValueError:
-            print("Invalid Input. <COMMIT_SELL USER_ID>")
-        else:    
-            cancel_sell(transactionNum, user_id, cursor, conn) 
-    elif command == "SET_SELL_TRIGGER":
-        try:
-            user_id, symbol, amount = arguments
-        except ValueError:
-            print("Invalid input. <SET_SELL_TRIGGER USER_ID STOCK_SYMBOL AMOUNT>")
-        else:
-            set_sell_trigger(transactionNum, user_id, symbol, amount, cursor, conn)
-    elif command == "DUMPLOG":
-        try:
-            [location] = arguments
-        except ValueError:
-            try:
-                user_id, filename = arguments
-            except ValueError: 
-                print("Invalid Input. Functionality: <DUMPLOG FILENAME> or <DUMPLOG USERNAME>")
-            else:
-                filename = os.path.basename(location)
-                path = os.path.join("/out/", filename)
-                dumplog_user(transactionNum, user_id, path)
-        else:
-            filename = os.path.basename(location)
-            path = os.path.join("/out/", filename)
-            dumplog(transactionNum, path)
-    elif command == "DISPLAY_SUMMARY":
-        try:
-            [user_id] = arguments
-        except ValueError:
-            print("Invalid input. <DISPLAY_SUMMARY USER_ID>")
-        else:
-            display_summary(transactionNum, user_id)
-    else:
-        print(arguments, " Invalid Command")
+            add(transactionNum, user_id, amount, conn)
 
 app = Flask(__name__)
 
-connections = Queue(10)
-
-time.sleep(10)
-#for i in range(0, 10):
-    #connections.put(create_connection())
-    #print("making")
-
+time.sleep(10) # hack for now to let database start up properly
 pool = psycopg2.pool.ThreadedConnectionPool(10, 20, user="postgres", password="supersecure", host="postgres", port="5432", database="postgres")
 
 @app.route('/', methods=['POST'])
@@ -170,11 +43,8 @@ def root():
     print(body, flush=True)
 
     conn = pool.getconn()
-    print("done " + body)
-    parseCommand(body, conn.cursor(), conn)
-    print("here2 " + body)
+    parseCommand(body, conn)
     pool.putconn(conn)
-    print("here1 " + body)
 
     response = jsonify(success=True)
     return response
